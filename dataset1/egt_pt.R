3dc90707be84fcbd17db07e3dba47c629f53a7a7
Yuanwei Yao <5612084+yaoyuanwei@users.noreply.github.com>
2023-07-26 15:30:03 +0200

    Beh modeling

---
+++ dataset1/egt_pt.R
@@ -0,0 +1,14 @@
+# Set parameters for the original perspective theory 
+
+egt_pt <- hBayesDM_egt(
+  task_name       = "egt",
+  model_name      = "pt",
+  model_type      = "",
+  data_columns    = c("subjID", "gain", "loss", "cost", "choice"),
+  parameters      = list(
+    "rho"     = c(0, 0.1, 1),
+    "lambda"  = c(0, 0.5, 5),
+    "beta"    = c(0, 1, 20)
+  ),
+  regressors      = NULL,
+  postpreds       = c("y_pred"))
